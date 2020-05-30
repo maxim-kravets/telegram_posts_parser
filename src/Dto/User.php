914c55f5f565269ede05344d8de2699abbbe4ff3
Maxim Kravets <max@deovr.com>
2020-05-30 03:45:47 +0300

    Fix User dto

--- src/Dto/User.php
+++ src/Dto/User.php
@@ -7,18 +7,18 @@ class User
     private $username;
     private $telegram_id;
 
-    public function __construct(string $username, int $telegram_id)
+    public function __construct($username, $telegram_id)
     {
         $this->username = $username;
         $this->telegram_id = $telegram_id;
     }
 
-    public function getUsername(): string
+    public function getUsername()
     {
         return $this->username;
     }
 
-    public function getTelegramId(): int
+    public function getTelegramId()
     {
         return $this->telegram_id;
     }
